ffccd3f671ab244d176aacd7892717673cbc1ded
Babichev Maxim <babichev@mail.tvil.ru>
2019-04-09 15:20:30 +0300

    patch slug & wheels screen

--- screens/WheelsScreen.js
+++ screens/WheelsScreen.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { Text, Button, ActivityIndicator, ScrollView, StyleSheet } from 'react-native';
+import {Text, Button, ActivityIndicator, ScrollView, StyleSheet, Image} from 'react-native';
 import { View, FlatList } from 'react-native';
-import Colors from '../constants/Colors'
+import Colors from '../constants/Colors';
+import { Icon } from 'expo';
 
 export default class WheelsScreen extends React.Component {
 
   static navigationOptions = {
     title: 'Wheels',
-    headerRight: (
-      <Button
-        onPress={() => alert('Hello, ReactNative!')}
-        title="Info"
-        color={Colors.tintColor}
-      />
-    ),
+    // headerRight: (
+    //   <Button
+    //     onPress={() => alert('Hello, ReactNative!')}
+    //     // title="Info"
+    //     color={Colors.tintColor}
+    //   >
+    //     <Icon
+    //       name="quote-right"
+    //       backgroundColor="transparent"
+    //       underlayColor="transparent"
+    //       color="black"
+    //       onPress={() => alert('Hello, ReactNative!')}
+    //     >
+    //       <Text style={{fontSize: 15}} />
+    //     </Icon>
+    //   </Button>
+    // ),
   };
 
   state = {
@@ -75,24 +86,68 @@ export default class WheelsScreen extends React.Component {
         data={this.state.dataSource}
         showsVerticalScrollIndicator={false}
         renderItem={({item}) =>
-          <View style={styles.flatView}>
-            <Text style={styles.name}>{item.name}</Text>
+          <View style={styles.titleContainer}>
+            <View style={styles.titleIconContainer}>
+              <Image
+                source={{ uri: 'https://s3.amazonaws.com/exp-brand-assets/ExponentEmptyManifest_192.png' }}
+                style={{ width: 128, height: 128 }}
+                resizeMode="cover"
+              />
+            </View>
+
+            <View>
+              <Text style={styles.nameText} numberOfLines={1}>
+                {item.name}
+              </Text>
+
+              <Text style={styles.slugText} numberOfLines={1}>
+                {item.name} {item.brand_id}
+              </Text>
+
+              <Text style={styles.descriptionText}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                culpa qui officia deserunt mollit anim id est laborum.
+              </Text>
+            </View>
           </View>
         }
         keyExtractor={item => item.id}
       />
-    );
-
-    return (
-      <ScrollView style={styles.container}>
-        {/*{!this.state.items.length && <ActivityIndicator size="small" color={Colors.tintColor} />}*/}
-      </ScrollView>
     );
   }
 
 }
 
 const styles = StyleSheet.create({
+  titleContainer: {
+    paddingHorizontal: 15,
+    paddingTop: 15,
+    paddingBottom: 15,
+    flexDirection: 'row',
+  },
+  titleIconContainer: {
+    marginRight: 15,
+    paddingTop: 2,
+  },
+  nameText: {
+    fontWeight: '600',
+    fontSize: 18,
+  },
+  slugText: {
+    color: '#a39f9f',
+    fontSize: 14,
+    backgroundColor: 'transparent',
+  },
+  descriptionText: {
+    flex: 1,
+    flexWrap: 'wrap',
+    fontSize: 14,
+    color: '#4d4d4d',
+  },
+
   container: {
     flex: 1,
     marginTop: 50,
