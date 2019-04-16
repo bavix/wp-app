56bfcdd71d27c143390f5898913630a85e71945c
Babichev Maxim <babichev@mail.tvil.ru>
2019-04-16 18:05:18 +0300

    add refreshAsync

--- helpers/AppAuth.js
+++ helpers/AppAuth.js
@@ -1,7 +1,7 @@
 import axio from './Axio'
 import {CLIENT_ID, CLIENT_SECRET} from '../constants/Config'
 
-const authAsync = async (config) => {
+const req = async (config) => {
   return await axio.post('oauth/token', {
     client_id: CLIENT_ID.toString(),
     client_secret: CLIENT_SECRET.toString(),
@@ -11,7 +11,7 @@ const authAsync = async (config) => {
 
 export const app = {
   async authAsync(options) {
-    return await authAsync({
+    return await req({
       grant_type: 'client_credentials',
       ...options,
     })
@@ -23,17 +23,26 @@ export const app = {
 
 export const client = {
   async authAsync(username, password, options) {
-    return await authAsync({
+    return await req({
       grant_type: 'password',
       username,
       password,
       ...options,
     })
   },
-  async refreshAsync(token) {
-    // todo
+  async refreshAsync(refresh_token, options) {
+    return await req({
+      grant_type: 'refresh_token',
+      refresh_token,
+      ...options,
+    })
   },
-  async revokeAsync(token) {
+  async revokeAsync(access_token, options) {
+    // return await req({
+    //   grant_type: 'revoke_token',
+    //   refresh_token,
+    //   ...options,
+    // })
     // todo
   },
 };
