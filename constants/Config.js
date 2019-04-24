c1872c68c7cc3eaeaff023aa1b5788441b39d4d5
Babichev Maxim <info@babichev.net>
2019-04-24 21:49:15 +0300

    update client secret

--- constants/Config.js
+++ constants/Config.js
@@ -1,3 +1,3 @@
 export const BASE_URL = 'https://wp.babichev.net/';
 export const CLIENT_ID = 1;
-export const CLIENT_SECRET = 'NAfpgncef0C5AQAPNhIw9tfNdj1USzIT2XhATREb';
+export const CLIENT_SECRET = 'qfPoAHKcY4A1fJ3xDuzjJyUa8wIe3gXWmOe5MCR5';
