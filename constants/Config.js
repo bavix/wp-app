5600b6806fa0148dd4b1f1b530e2b6ae7a71dab5
Babichev Maxim <babichev@mail.tvil.ru>
2019-04-30 16:35:45 +0300

    fix client_secret

--- constants/Config.js
+++ constants/Config.js
@@ -1,3 +1,3 @@
 export const BASE_URL = 'https://wp.babichev.net/';
 export const CLIENT_ID = 1;
-export const CLIENT_SECRET = 'qfPoAHKcY4A1fJ3xDuzjJyUa8wIe3gXWmOe5MCR5';
+export const CLIENT_SECRET = 'ayH7hHNCL6iKIxT62cx6zK8WiihPb5OXI2rnw3hT';
